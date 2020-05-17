e80aa9b09732546f954301528ea8b6e5edbaefd6
loloburgwardt <loloburgwardt@burgwardt.com.ar>
2020-05-17 19:57:29 -0300

    Avanzamos con el ruteo

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require ("express");
+const app = express();
+const path = require("path");
+
+
+//SETEAR ARCHIVOS ESTÁTICOS , CSS , IMAGENES , ETC. 
+app.use(express.static(__dirname + '/public'));
+
+//SETEAR MOTOR GRÁFICO (EJS)
+app.set("views", path.join(__dirname,"/views"));
+app.set("view engine","ejs");
+
+//CAPTURAR DATOS QUE RECIBAMOS POR POST, ALMACENARNOS EN FORMA DE OBJETO LITERAL Y PODER GUARDARLOS EN FORMATO JSON
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// RUTAS !
+const homeRouter= require("./routes/homeRouter")
+app.get ("/", homeRouter);
+
+
+
+
+
+
+// EMPEZAR SERVER
+app.listen(3030,()=>{console.log("SV running")})
