ec4ac178f621dd7fe51ae3375ca39c285305cbaa
GastonBerretta <gastonrberretta@gmail.com>
2020-07-19 03:29:53 -0300

    Pruebas en el login react

--- Dashboard/src/components/Login.js
+++ Dashboard/src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+ import React, { useState } from 'react';
 
 class Login extends React.Component {
     constructor(props) {
@@ -18,4 +18,51 @@ class Login extends React.Component {
     
 }
 
+export default Login; 
+/* import React, { useState } from 'react';
+
+class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            email:"gastonrberretta@gmail.com",
+            password:"123123123"
+        }
+      }
+      handleEmail(text){
+          this.setState({email:text.target.value})
+      }
+      handlePassword(text){
+        this.setState({password:text.target.value})
+    }
+    componentDidMount(){
+        let obj = {}
+        obj.email=this.state.email;
+        obj.password= this.state.password
+        fetch('http://localhost:3030/api/login', {
+            header:{
+                "content-Type": "application/json"
+            },
+            method: 'POST',
+            body: {email: this.state.email,
+                password: this.state.password}})
+        .then(res => console.log(res))
+    }
+    render() {
+        return (
+            
+            <React.Fragment>
+               <form >
+                   {console.log(this.state)} 
+                    <input type = "text"  placeholder = 'Email' onChange={(text)=> {this.handleEmail(text)}} /> 
+                    <input type= "password" placeholder = 'Contraseña' onChange={(text)=> {this.handlePassword(text)}}/>
+                    <button onMouseOver={()=> {this.login()}} >Login</button>
+                </form>
+            </React.Fragment>
+        )
+    }
+    
+}
+
 export default Login;
+ */
